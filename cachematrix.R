1c129f4cceddae384de69c133346a361e00520b2
Vladimir Vishnevsky <vvishnevsky@vvishnevsky.oilspace.net>
2014-09-16 15:57:42 +0400

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates complex variable which can:
+## 1. hold matrix value(aka aMatrix variable)
+## 2. hold inverse of the matrix (aSolvedMatrix)
+## supported operations:
+## -set original matrix (makeCacheMatrix's set)
+## -get original matrix (makeCacheMatrix's get)
+## -set inverse of aMatrix (makeCacheMatrix's setCached)
+## -get inverse of aMatrix (makeCacheMatrix's getCached)
 
+makeCacheMatrix <- function(aMatrix = matrix()) {
+    aSolvedMatrix <- NULL;
+    set <- function(y) {
+        aMatrix <<- y;
+        aSolvedMatrix <<- NULL;
+    };
+    get <- function() aMatrix;
+    setCached <- function(mean) aSolvedMatrix <<- mean;
+    getCached <- function() aSolvedMatrix;
+    list(set = set, get = get,
+         setCached = setCached,
+         getCached = getCached);
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve finds inverse value(matrix) of supplied matrix.
+## Caches solved value to avoid complex computations, 
+## so real solving happens on first call only.
+## precondition:
+## aMatrix must be created with appropriate makeCacheMatrix call
+cacheSolve <- function(aMatrix, ...) {
+    ## Return a matrix that is the inverse of 'aMatrix'
+    aSolvedMatrix <- aMatrix$getCached();
+    if(!is.null(aSolvedMatrix)) {
+        message("getting cached data");
+        return(aSolvedMatrix);
+    }
+    data <- aMatrix$get();
+    aSolvedMatrix <- solve(data, ...);
+    aMatrix$setCached(aSolvedMatrix);
+    aSolvedMatrix;
 }
